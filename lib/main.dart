d59b078d29295be748ea7d0a7fc72a9512871a37
Benjamin Netanyahu <netanyahubenjamin70@gmail.com>
2024-10-21 11:21:39 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+    // loading: ,
+    // location: ,
+  ));
+}
