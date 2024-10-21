a3db0453a5571d262d3937d35172e3fd7f8412d3
Benjamin Netanyahu <netanyahubenjamin70@gmail.com>
2024-10-21 11:42:11 +0300

    "Updated main.dart to use named routes and added ChooseLocation and Loading pages. Modified ChooseLocation and Home pages to include AppBar and TextButton."

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-    // loading: ,
-    // location: ,
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
 }
